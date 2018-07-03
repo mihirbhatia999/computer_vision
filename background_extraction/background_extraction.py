ba5d678dc280d72b1c064f27d2ff299e9e5667b7
Mihir Bhatia <bhatia35@purdue.edu>
2018-07-03 15:44:35 -0400

    Update background_extraction.py

--- background_extraction/background_extraction.py
+++ background_extraction/background_extraction.py
@@ -24,38 +24,39 @@ while(1):
         break
 cap.release
 cv2.destroyAllWindows()
+
 #condition where the pixel always remains black. Just extract all those pixel values
 
-##bgd = []
-##for  bgd in glob.glob('C:/Users/Mihir/Desktop/Purdue IE/Fall 2017/Academics/IE590/Homework/homework 2/BGD/*'):
-##    img = cv2.imread(bgd,1)
-##    bgd.append(img)
-##
-##color = []
-##for  color in glob.glob('C:/Users/Mihir/Desktop/Purdue IE/Fall 2017/Academics/IE590/Homework/homework 2/Color/*'):
-##    img2 = cv2.imread(color,1)
-##    color.append(img2) 
-##                
-##height, width, channels = frame.shape
-##
-##background = np.zeros((height,width))
-##
-##for px in range(0,height) :
-##    
-##    for py range(0,width) :         #pixel chosen with coordinates (px,py) 
-##
-##        for j in range(100,i):              #image chosen
-##
-##            for k in range( j - 3 , j +3 ):     #analysising few images before and after to check if the pixel is actually background
-##
-##                sum  =  sum + bgd[ j ][px,py]
-##                
-##            if( sum  ==  0 ):
-##                background[ px , py ] = color [ j ][px , py]
-##
-##            sum=0
-##
-##cv2.imshow('background' , background)
+bgd = []
+for  bgd in glob.glob('C:/Users/Mihir/Desktop/Purdue IE/Fall 2017/Academics/IE590/Homework/homework 2/BGD/*'):
+    img = cv2.imread(bgd,1)
+    bgd.append(img)
+
+color = []
+for  color in glob.glob('C:/Users/Mihir/Desktop/Purdue IE/Fall 2017/Academics/IE590/Homework/homework 2/Color/*'):
+    img2 = cv2.imread(color,1)
+    color.append(img2) 
+                
+height, width, channels = frame.shape
+
+background = np.zeros((height,width))
+
+for px in range(0,height) :
+    
+    for py range(0,width) :         #pixel chosen with coordinates (px,py) 
+
+        for j in range(100,i):              #image chosen
+
+            for k in range( j - 3 , j +3 ):     #analysising few images before and after to check if the pixel is actually background
+
+                sum  =  sum + bgd[ j ][px,py]
+                
+            if( sum  ==  0 ):
+                background[ px , py ] = color [ j ][px , py]
+
+            sum=0
+
+cv2.imshow('background' , background)
 
 
                 
